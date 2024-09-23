b480a90d3f5af06c65f7fbb5798051a72107949c
angad <angadkanojia@gmial.com>
2024-09-23 15:28:54 +0530

    Feat : add vscode setting

--- src/app/api/user-details/route.ts
+++ src/app/api/user-details/route.ts
@@ -1,30 +1,32 @@
-
-import { NextResponse } from 'next/server';
-import { prisma } from '../db'; 
-
+import { NextResponse } from "next/server";
+import { prisma } from "../db";
 
 export async function POST(request: Request) {
   try {
     const { username, password } = await request.json();
 
-    console.log(username,password)
+    console.log(username, password);
     if (!username || !password) {
-      return NextResponse.json({ message: 'Username and password are required.' }, { status: 400 });
+      return NextResponse.json(
+        { message: "Username and password are required." },
+        { status: 400 }
+      );
     }
 
-    
-    const user = await prisma.user.findUnique({
+    const user = await prisma.user.findFirst({
       where: { username },
     });
 
-    
-    if (user && password=== user.password) {
-      return NextResponse.json({ message: 'Login successful!' });
+    if (user && password === user.password) {
+      return NextResponse.json({ message: "Login successful!" });
     } else {
-      return NextResponse.json({ message: 'Invalid username or password.' }, { status: 401 });
+      return NextResponse.json(
+        { message: "Invalid username or password." },
+        { status: 401 }
+      );
     }
   } catch (error: any) {
-    console.error('Failed to login:', error);
-    return NextResponse.json({ error: 'Failed to login' }, { status: 500 });
+    console.error("Failed to login:", error);
+    return NextResponse.json({ error: "Failed to login" }, { status: 500 });
   }
 }
